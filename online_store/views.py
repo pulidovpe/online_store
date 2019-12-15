09f2bfd3393ce4824fc599377f7c84cce8d1c1e5
Pulido V. P. E <pulidovpe.dev@gmail.com>
2019-12-14 20:32:34 -0400

    Templates, render, variables

--- online_store/views.py
+++ online_store/views.py
@@ -1,4 +1,11 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse('Hola Mundo!')
+	##return HttpResponse('Hola Mundo!')
+	return render(request,'index.html',{
+		'message0': 'Nuevo mensaje (de contexto) desde la vista',
+		'message1': 10 + 20,
+		'message2': [1,2,3],
+		'message3': True,
+	})
